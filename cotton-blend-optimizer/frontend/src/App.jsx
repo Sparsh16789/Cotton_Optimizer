4dcf1979b8f7becdfdd87d8e9630d7e54b868a3d
Sparsh Gupta <153621269+Sparsh16789@users.noreply.github.com>
2026-06-17 15:08:09 +0530

    Update App.jsx

--- cotton-blend-optimizer/frontend/src/App.jsx
+++ cotton-blend-optimizer/frontend/src/App.jsx
@@ -115,7 +115,7 @@ export default function App() {
     };
 
     try {
-      const res = await fetch("https://cotton-backend-l2s5.onrender.com//optimize", {
+      const res = await fetch("https://cotton-backend-l2s5.onrender.com/optimize", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
